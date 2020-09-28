44d5afa83d52c6137e0def37f29eae068855e187
Du <1034331038@qq.com>
2020-09-28 23:58:19 +0800

    Create TestHashMap.java

---
+++ src/hashmap_test/TestHashMap.java
@@ -0,0 +1,11 @@
+package hashmap_test;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestHashMap {
+    public static void main(String[] args) {
+        HashMap<Integer,String> hs=new HashMap<>();
+        ConcurrentHashMap
+    }
+}
